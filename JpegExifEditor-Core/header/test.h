69e7923341a8bd0afb26014ca167f70d6c8a1cdb
Hansuck Shim <HansuckShim@Hansuckui-MacBook-Air.local>
2017-09-02 19:13:31 +0900

    add test files to check xcode is how working with git

---
+++ JpegExifEditor-Core/header/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  JpegExifEditor-Core
+//
+//  Created by gaucho1218 on 2017. 9. 2..
+//  Copyright © 2017 gaucho1218. All rights reserved.
+//
+
+#ifndef test_h
+#define test_h
+
+#include <stdio.h>
+
+#endif /* test_hpp */
