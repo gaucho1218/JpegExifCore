afdac7e524dece408e031d61f60d7866d8154a8a
gaucho1218 <gaucho1218@gmail.com>
2017-09-03 20:06:19 +0900

    add JpegParser to project and delete test files

---
+++ JpegExifEditor-Core/header/JpegParser.h
@@ -0,0 +1,14 @@
+//
+//  JpegParser.h
+//  JpegExifEditor-Core
+//
+//  Created by gaucho1218 on 2017. 9. 3..
+//  Copyright © 2017 gaucho1218. All rights reserved.
+//
+
+#ifndef JPEG_PARSER_H
+#define JPEG_PARSER_H
+
+
+
+#endif
--- JpegExifEditor-Core/source/test.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  test.cpp
-//  JpegExifEditor-Core
-//
-//  Created by gaucho1218 on 2017. 9. 2..
-//  Copyright © 2017년 gaucho1218. All rights reserved.
-//
-
-#include "test.h"
