2332537317d84550556c35274a5999dba3bf7eac
gaucho1218 <gaucho1218@gmail.com>
2017-09-13 23:02:37 +0900

    Done for today

--- JpegExifEditor-Core/source/JpegParser.cpp
+++ JpegExifEditor-Core/source/JpegParser.cpp
@@ -11,8 +11,14 @@
 TJpegInfo ParseJpegData(const char *const pBuf, const int nSize)
 {
     TJpegInfo tRet = std::make_tuple(EJpegHdrType::EJPEG_NONE, 0);
+ 
+    if( pBuf == nullptr || nSize <= 0 )
+        return tRet;
 
-
-
+    
+    //! need effective way to parse JPEG header
+    const short *const pHdr = reinterpret_cast<short *>(const_cast<char *>(pBuf));
+    
+    
 	return tRet;
 }
