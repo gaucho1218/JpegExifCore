9d29b020edf82a79bb6f979d092689ed65701b48
gaucho1218 <gaucho1218@gmail.com>
2017-09-17 15:43:23 +0900

    add xcode base sample

---
+++ Sample/JpegParserSample/JpegParserSample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  JpegParserSample
+//
+//  Created by Hansuck Shim on 2017. 9. 17..
+//  Copyright © 2017년 gaucho1218. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
