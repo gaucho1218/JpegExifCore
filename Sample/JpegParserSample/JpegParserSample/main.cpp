2deb39a32e65bc85ffeceb79fc8ac30bdaed6c90
gaucho1218 <gaucho1218@gmail.com>
2017-09-17 23:32:08 +0900

    could load sample jpeg file. test library later!

--- Sample/JpegParserSample/JpegParserSample/main.cpp
+++ Sample/JpegParserSample/JpegParserSample/main.cpp
@@ -12,6 +12,12 @@
 
 int main(int argc, const char * argv[]) {
     
+    FILE *pFile = fopen("../sample_beer.jpg", "rb");
+    if( pFile == nullptr )
+    {
+        printf("Open Failed\n");
+    }
+    
     auto temp = ParseJpegData(nullptr, 0);
     std::get<EJI_HDR>(temp);
     
