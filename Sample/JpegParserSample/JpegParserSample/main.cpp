8269eb5fc5b291430442679007df636096935521
gaucho1218 <gaucho1218@gmail.com>
2017-09-17 16:22:13 +0900

    default sample setting

--- Sample/JpegParserSample/JpegParserSample/main.cpp
+++ Sample/JpegParserSample/JpegParserSample/main.cpp
@@ -2,14 +2,17 @@
 //  main.cpp
 //  JpegParserSample
 //
-//  Created by Hansuck Shim on 2017. 9. 17..
+//  Created by gaucho1218 on 2017. 9. 17..
 //  Copyright © 2017년 gaucho1218. All rights reserved.
 //
 
 #include <iostream>
 
+#include "JpegParser.h"
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    
+    
     return 0;
 }
