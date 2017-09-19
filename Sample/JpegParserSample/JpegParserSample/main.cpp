e126825a65325e37c22ffddaa553874ee98a74d6
gaucho1218 <gaucho1218@gmail.com>
2017-09-19 23:51:47 +0900

    found logic bug -> why I thought like that;;;

--- Sample/JpegParserSample/JpegParserSample/main.cpp
+++ Sample/JpegParserSample/JpegParserSample/main.cpp
@@ -18,8 +18,63 @@ int main(int argc, const char * argv[]) {
         printf("Open Failed\n");
     }
     
-    auto temp = ParseJpegData(nullptr, 0);
-    std::get<EJI_HDR>(temp);
+    auto nSize = 1024;
+    auto pBufStart = malloc(nSize);
+    if( pBufStart == nullptr )
+    {
+        printf("Couldn't allocate memory!!!\n");
+        return -1;
+    }
+    
+    char *pCurBuf = reinterpret_cast<char *>(pBufStart);
+    auto bEOI{false};
+    
+    while(!bEOI)
+    {
+        auto nReadSize = fread(pBufStart, 1, nSize, pFile);
+        
+        while(bEOI == false)
+        {
+            auto outTuple = ParseJpegData(pCurBuf, static_cast<int>(nReadSize));
+            
+            if( std::get<EJI_HDR>(outTuple) != EJPEG_NONE )
+            {
+                printf("Type: 0x%02X\t", std::get<EJI_HDR>(outTuple));
+                printf("Offset: %d\t", std::get<EJI_OFFSET>(outTuple));
+                printf("Size: %d\n", std::get<EJI_SIZE>(outTuple));
+                
+                if( std::get<EJI_HDR>(outTuple) == EJPEG_EOI )
+                {
+                    bEOI = true;
+                    break;
+                }
+                
+                if( std::get<EJI_SIZE>(outTuple) == 0 )
+                {
+                    pCurBuf += 2;
+                    nReadSize -= 2;
+                }
+                else if( std::get<EJI_SIZE>(outTuple) > (nReadSize +2) )
+                {
+                    //! need to read more data from file
+                    pCurBuf += nReadSize;
+                    nReadSize = 0;
+                    break;
+                }
+                else
+                {
+                    auto nCurDataSize = std::get<EJI_SIZE>(outTuple) + 2;
+                    pCurBuf += nCurDataSize;
+                    nReadSize -= nCurDataSize;
+                }
+            }
+            else
+            {
+                bEOI = true;
+                break;
+            }
+        }   //! end of while ParseJpegData()
+    }   //! end of while fread()
     
     return 0;
 }
