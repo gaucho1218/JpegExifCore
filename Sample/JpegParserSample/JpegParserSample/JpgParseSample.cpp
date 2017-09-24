5ce139daf0c76a3828c8347e5c3eb6977f938a34
gaucho1218 <gaucho1218@gmail.com>
2017-09-24 14:25:40 +0900

    thinking about add class in sample project change line setting to CRLF due to it is more reasonable for network either

---
+++ Sample/JpegParserSample/JpegParserSample/JpgParseSample.cpp
@@ -0,0 +1,14 @@
+//
+//  JpgParseSample.cpp
+//  JpegParserSample
+//
+//  Created by gaucho1218 on 2017. 9. 24..
+//  Copyright © 2017년 gaucho1218. All rights reserved.
+//
+
+#include "JpgParseSample.h"
+
+void JpegHdrFullParse(void)
+{
+    
+}
---
+++ Sample/JpegParserSample/JpegParserSample/JpgParseSample.h
@@ -0,0 +1,14 @@
+//
+//  JpgParseSample.h
+//  JpegParserSample
+//
+//  Created by gaucho1218 on 2017. 9. 24..
+//  Copyright © 2017년 gaucho1218. All rights reserved.
+//
+
+#ifndef JpgParseSample_h
+#define JpgParseSample_h
+
+void JpegHdrFullParse(void);
+
+#endif /* JpgParseSample_h */
