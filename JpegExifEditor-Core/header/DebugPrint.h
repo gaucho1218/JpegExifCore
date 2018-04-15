da1d7da596998a686825fbaf901033779d3e821c
gaucho1218 <gaucho1218@gmail.com>
2018-04-16 00:43:12 +0900

    #10 test complete
    -> need to test this probem caused by DLL

--- JpegExifEditor-Core/header/DebugPrint.h
+++ JpegExifEditor-Core/header/DebugPrint.h
@@ -11,7 +11,11 @@
 
 #if defined(DEBUG) || defined(_DEBUG)
 #include <stdio.h>
+#ifdef _WIN32
 #define JPDebugPrint(fmt, args, ...)  fprintf(stderr, "[%s, %d]: " fmt, __FILE__, __LINE__, ##args)
+#else
+#define JPDebugPrint(fmt, args...)  fprintf(stderr, "[%s, %d]: " fmt, __FILE__, __LINE__, ##args)
+#endif
 #else
 DebugPrint(...)
 #endif
